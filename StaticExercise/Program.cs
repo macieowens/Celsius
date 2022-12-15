d3bfcc7aa82854a6a025a2122dc5b7034af1a17b
macieowens <112588931+macieowens@users.noreply.github.com>
2022-12-15 15:00:24 -0600

    inital commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StaticExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine($"After conversion");
+            Console.WriteLine($"fahreinheit {fahrenheit}");
+            Console.WriteLine($"celsius {celsius}");
+        }
+    }
+}
